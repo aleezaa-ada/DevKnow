c8a00526b46ecc1ea854103fe00cad445d7d36a5
Aleeza Ahmed <aleezaahmed@deloitte.co.uk>
2026-05-01 13:37:05 +0100

    feat(settings): add CORS support for localhost and ensure middleware is correctly configured

--- DevKnow-backend/config/settings.py
+++ DevKnow-backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,4 +136,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
